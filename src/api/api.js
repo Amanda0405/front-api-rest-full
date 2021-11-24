71d26414dd24ebaccff46fbfbf87cc4a2bca6de6
Amanda Silva <89053786+Amanda0405@users.noreply.github.com>
2021-11-23 21:39:35 -0300

    Add files via upload

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+const Api = {
+  apiUrl: 'https://back-end-api-restfull.herokuapp.com/infantis',
+  fetchGetAll: () => fetch(`${Api.apiUrl}/listAll`),
+  fetchGetById: id => fetch(`${Api.apiUrl}/listId/${id}`),
+  fetchPost: (vaga) => {
+    return fetch(`${Api.apiUrl}/add`, {
+      method: 'POST',
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(vaga)
+    })
+  },
+  fetchPut: (vaga, id) => {
+    return fetch(`${Api.apiUrl}/update/${id}`, {
+      method: 'PUT',
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify(vaga)
+    })
+  },
+  fetchDelete: (id) => {
+    return fetch(`${Api.apiUrl}/delete/${id}`, {
+      method: 'DELETE'
+    })
+  }
+}
+
+export default Api;
